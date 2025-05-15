e5d8188653dff9cde1b78db31089762ecc11c219
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-05-15 20:30:53 +0300

    init go project

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/movies", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "Bravo!",
+		})
+	})
+
+	server.Run() // 8080
+}
