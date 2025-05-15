586c7406e908b88de4440334b879576bd284a4eb
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-05-16 00:22:06 +0300

    make tests for return data (just take a look)

--- services/movie-service.go
+++ services/movie-service.go
@@ -1,13 +1,27 @@
 package services
 
 import (
+	dbconnection "movies-svc-V2/db-connection"
+	databasetypes "movies-svc-V2/models/database-types"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-func GetMovieDetails(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"data": "The Witcher: Season 4 + VIDEO URL",
-	})
+func GetMovieDetails(c *gin.Context) {
+	movieTitle := "Anora" // или можеш да вземеш от параметър: c.Param("title")
+
+	var movie databasetypes.Movie
+
+	err := dbconnection.DB.
+		Preload("CastList").
+		Preload("Images").
+		Where("title = ?", movieTitle).First(&movie).Error
+
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, movie)
 }
