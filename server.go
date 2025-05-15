b6c079fb4fe974437c7f787546086ee295e0ce9f
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-05-16 00:04:23 +0300

    order code

--- server.go
+++ server.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"log"
+
+	dbconnection "movies-svc-V2/db-connection"
 	"movies-svc-V2/routes"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
 func main() {
 	server := gin.Default()
+
+	_, err := dbconnection.ConnectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	routes.ModularRoute(server)
 	server.Run() // PORT 8080
 }
