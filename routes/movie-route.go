c5b4660ca4ad01e98efbb601348acea9fe987105
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-05-17 02:39:21 +0300

    add more routers

--- routes/movie-route.go
+++ routes/movie-route.go
@@ -7,5 +7,15 @@ import (
 )
 
 func MovieRoute(server *gin.Engine) {
-	server.GET("/movie/The-Witcher", services.GetMovieDetails)
+
+	// GET DATA (Just to consume data!)
+	server.GET("/get-movie-details", services.GetMovieDetails)
+	server.GET("/get-all-movies-count", services.GetAllMoviesCount)
+	server.GET("/get-next-thirty-movies", services.GetNextThirtyMovies)
+	server.GET("/get-movies-count-by-genre", services.GetAllMoviesCountByGenre)
+	server.GET("/get-next-twenty-movies-by-genre", services.GetAllMoviesByGenre)
+	server.GET("/get-movies-by-title", services.GetMoviesByTitle)
+	server.GET("/get-searched-movies-count", services.GetMoviesCountByTitle)
+
+	// CREATE DATA (Create records in database!)
 }
