00379b92b837629897c29887f057123b3c3f503c
Antoan Metodiev <131536878+antoanMetodiev@users.noreply.github.com>
2025-05-17 02:38:32 +0300

    add all consume movies operations from v1

--- server.go
+++ server.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"log"
+	"time"
 
 	dbconnection "movies-svc-V2/db-connection"
 	"movies-svc-V2/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
 func main() {
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	_, err := dbconnection.ConnectToDB()
 	if err != nil {
